588160e419243501e2547f06df967d7ce7d85095
kappiahdh <koad.junior@gmail.com>
2026-09-21 22:34:09 -0400

    Add Kwabena's line. (Changed on seperate branch)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         story[0] = "Once upon a time, a cloud with an angry face started bringing rain and thunder over a city, and then";
 
         // Line 2 Kwabena or Angel
-        story[1] = "The city was starting to flood and the people were drenched from the heavy rain, and then";
+        story[1] = "The city was starting to flood and the people were drenched from the heavy thunderstorm, and then";
 
         // Line 3 Kwabena or Angel
         story[2] = null;
